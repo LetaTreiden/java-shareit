81e88700c927427df1f34b40657a629fdd9aed2b
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2022-10-03 02:58:10 +0300

    unused imports

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -5,15 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query(value = "SELECT i FROM Item i WHERE i.owner.id = ?1")
     Collection<Item> findAllItemsByOwner(Long id);
 
-    @Query("select i from Item i" +
-            " where upper(i.name) like upper(concat('%', ?1, '%'))" +
-            " or upper(i.description) like upper(concat('%', ?1, '%'))" )
+    @Query("select i from Item i" + " where upper(i.name) like upper(concat('%', ?1, '%'))" + " or upper(i.description) like upper(concat('%', ?1, '%'))")
     Collection<Item> search(String text);
 }
