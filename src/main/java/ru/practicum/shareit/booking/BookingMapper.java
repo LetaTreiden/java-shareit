76c23654e3dc11113cc66c986b2657ec5dfc3946
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2022-11-10 13:51:38 +0300

    corrections

--- src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -1,10 +1,8 @@
 package ru.practicum.shareit.booking;
 
 import org.springframework.stereotype.Component;
-import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.dto.BookingDTO;
-import ru.practicum.shareit.item.ItemMapper;
-import ru.practicum.shareit.user.UserMapper;
+import ru.practicum.shareit.booking.model.Booking;
 
 @Component
 public class BookingMapper {
@@ -20,9 +18,9 @@ public class BookingMapper {
                 .build();
     }
 
-    public static Booking toBooking(BookingDTO bookingDto){
+    public static Booking toBooking(BookingDTO bookingDto) {
         Booking booking = new Booking();
-        if(bookingDto.getId() != null){
+        if (bookingDto.getId() != null) {
             booking.setId(bookingDto.getId());
         }
 
@@ -31,7 +29,7 @@ public class BookingMapper {
         booking.setItem(bookingDto.getItem());
         booking.setBooker(bookingDto.getBooker());
 
-        if(bookingDto.getBookingStatus() == null){
+        if (bookingDto.getBookingStatus() == null) {
             booking.setStatus(BookingStatus.WAITING);
         } else {
             booking.setStatus(bookingDto.getBookingStatus());
