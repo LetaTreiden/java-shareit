db23d8b672634906fd6c791249d4531e2f1ad558
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2022-11-12 18:34:25 +0300

    ch s

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -214,7 +214,7 @@ public class ItemServiceImpl implements ItemService {
                     commentTempDto.setAuthor(null);
                     commentTempDto.setItem(null);
                     return commentTempDto;
-                } else if(!booking.getStatus().equals(BookingStatus.PAST)) {
+                } else if (!booking.getStatus().equals(BookingStatus.PAST)) {
                     throw new InvalidParameterException("ронирование должно быть завершено");
                 } else {
                     throw new InvalidParameterException("Проверьте заданные параметры");
