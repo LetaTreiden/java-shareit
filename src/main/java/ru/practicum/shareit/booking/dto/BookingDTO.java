5c840940c30a36f8815ac5837f50dc1471c60cee
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2022-12-08 17:53:48 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDTO.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDTO.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.*;
 import ru.practicum.shareit.booking.BookingStatus;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
