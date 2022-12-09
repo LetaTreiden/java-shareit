01843008c1d8381264f0ce44ba72a6f11b713a81
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2022-12-09 13:46:26 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -2,12 +2,12 @@ package ru.practicum.shareit.item.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
-@Repository
+//@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query(value = "SELECT i FROM Item i WHERE i.owner.id = ?1")
