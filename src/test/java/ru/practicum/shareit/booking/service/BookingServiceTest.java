c0188a36dccee367e96d59792ea84552662ce664
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-02-13 03:53:21 +0300

    Add files via upload

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceTest.java
@@ -36,7 +36,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.ARRAY;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 
