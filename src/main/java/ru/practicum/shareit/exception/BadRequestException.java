c7941aa560e9ab9a6ca94ea6733e1100e8917976
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-02-15 01:39:59 +0300

    Add files via upload

---
+++ src/main/java/ru/practicum/shareit/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exception;
+
+public class BadRequestException extends IllegalArgumentException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
