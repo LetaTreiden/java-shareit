94aef871d57bf916fa175df981e4ef3d87687561
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-02-16 00:29:26 +0300

    Add files via upload

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.request;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.RequestDTO;
 import ru.practicum.shareit.request.dto.RequestDTOWithItems;
