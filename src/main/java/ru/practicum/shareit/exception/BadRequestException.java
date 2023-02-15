ab4409dd7814f1c23da4522cf52f09550175945d
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-02-15 11:44:51 +0300

    Delete src directory

--- src/main/java/ru/practicum/shareit/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class BadRequestException extends IllegalArgumentException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
