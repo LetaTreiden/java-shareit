11ab3dee8162e6bce502474088215f2dd0be9d7f
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-02-16 01:25:01 +0300

    Add files via upload

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandlerGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandlerGateway.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
