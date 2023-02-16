11556bd0311ee8c8412b947ec67a595e5a487292
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-02-17 00:05:16 +0300

    Add files via upload

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserDtoGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDtoGateway.java
@@ -1,9 +1,12 @@
 package ru.practicum.shareit.user.dto;
 
 import lombok.Data;
+import ru.practicum.shareit.Create;
+import ru.practicum.shareit.Update;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
@@ -12,11 +15,9 @@ public class UserDtoGateway {
     private long id;
 
     @NotBlank
-    @Size(min = 1, max = 100)
     private String name;
 
-    @Email
-    @NotBlank
+    @Email(groups = {Create.class, Update.class})
+    @NotEmpty(groups = {Create.class})
     private String email;
-
 }
