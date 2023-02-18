4398f4f67900b033d45df651adbed533c7b3982c
Leta Treiden <97164609+LetaTreiden@users.noreply.github.com>
2023-02-18 20:28:28 +0300

    Add files via upload

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoGateway.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
